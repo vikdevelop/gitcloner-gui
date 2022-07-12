ad591a97e55ad051081f1244aeb3045129653c3b
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-12 06:24:41 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,110 @@
+import gi
+import os
+import sys
+import subprocess
+gi.require_version("Gtk", "4.0")
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
+HOME = os.path.expanduser('~')
+cs = 'LANG=cs_CZ.UTF-8\nLC_CTYPE="cs_CZ.UTF-8"\nLC_NUMERIC="cs_CZ.UTF-8"\nLC_TIME="cs_CZ.UTF-8"\nLC_COLLATE="cs_CZ.UTF-8"\nLC_MONETARY="cs_CZ.UTF-8"\nLC_MESSAGES="cs_CZ.UTF-8"\nLC_PAPER="cs_CZ.UTF-8"\nLC_NAME="cs_CZ.UTF-8"\nLC_ADDRESS="cs_CZ.UTF-8"\nLC_TELEPHONE="cs_CZ.UTF-8"\nLC_MEASUREMENT="cs_CZ.UTF-8"\nLC_IDENTIFICATION="cs_CZ.UTF-8"\nLC_ALL='
+if subprocess.getoutput("locale") == cs:
+    from translations.cs import *
+else:
+    from translations.en import *
+class Dialog_successfully(Gtk.Dialog):
+    def __init__(self, parent):
+        super().__init__(transient_for=parent, use_header_bar=True)
+        self.parent = parent
+
+        self.set_title(title=repository_is_cloned)
+        self.use_header_bar = True
+        self.set_modal(modal=True)
+        self.connect('response', self.dialog_response)
+
+        self.add_buttons(
+            close, Gtk.ResponseType.CANCEL,
+            open_folder, Gtk.ResponseType.OK,
+        )
+
+        btn_ok = self.get_widget_for_response(
+            response_id=Gtk.ResponseType.OK,
+        )
+        btn_ok.get_style_context().add_class(class_name='suggested-action')
+        btn_cancel = self.get_widget_for_response(
+            response_id=Gtk.ResponseType.CANCEL,
+        )
+        btn_cancel.get_style_context().add_class(class_name='destructive-action')
+
+        content_area = self.get_content_area()
+        content_area.set_orientation(orientation=Gtk.Orientation.VERTICAL)
+        content_area.set_spacing(spacing=12)
+        content_area.set_margin_top(margin=12)
+        content_area.set_margin_end(margin=12)
+        content_area.set_margin_bottom(margin=12)
+        content_area.set_margin_start(margin=12)
+
+        label = Gtk.Label()
+        label.set_markup(repository_was_cloned_successfully_message)
+        content_area.append(child=label)
+
+        self.show()
+
+    def dialog_response(self, dialog, response):
+        if response == Gtk.ResponseType.OK:
+            os.system("xdg-open %s/Gits" % HOME)
+            dialog.close()
+
+        elif response == Gtk.ResponseType.CANCEL:
+            dialog.close()
+
+    def get_entry_text(self):
+        return self.entry.get_text()
+
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_default_size(650, 350)
+        self.set_title(git_cloner_title)
+        headerbar = Gtk.HeaderBar.new()
+        
+        self.set_titlebar(titlebar=headerbar)
+        self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=15)
+        self.set_child(self.box1)
+        
+        self.img = Gtk.Image()
+        self.img.set_from_file("icon/git.png")
+        self.img.set_pixel_size(100)
+        self.box1.append(self.img)
+        
+        self.label = Gtk.Label()
+        self.label.set_markup(git_cloner_description)
+        self.box1.append(self.label)
+        
+        self.entry_clone = Gtk.Entry()
+        self.entry_clone.set_placeholder_text(enter_url)
+        self.box1.append(self.entry_clone)
+        
+        self.buttonC = Gtk.Button(label=clone)
+        self.buttonC.connect("clicked", self.on_buttonC_clicked)
+        self.box1.append(self.buttonC)
+        
+    def on_buttonC_clicked(self, widget, *args):
+        self.clone()
+    def clone(self):
+        entryc = self.entry_clone.get_text()
+        if not os.path.exists("%s/Gits" % HOME):
+            os.mkdir("%s/Gits" % HOME)
+        os.chdir("%s/Gits" % HOME)
+        os.popen("git clone %s" % entryc)
+        dialog_s = Dialog_successfully(self)
+        
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+    
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+app = MyApp(application_id="com.github.vikdevelop.scrcpy-gui")
+app.run(sys.argv)
---
+++ translations/cs.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# czech translations of package gitcloner-gui
+
+repository_is_cloned = 'Repozitář byl naklonován!'
+close = "_Zavřít"
+open_folder = "_Otevřít složku"
+repository_was_cloned_successfully_message = "Repozitář byl úspěšně naklonován! Pokud chcete, můžete otevřít složku nově vytvořené aplikace kliknutím na tlačítko 'Otevřít složku'."
+git_cloner_title = "Git cloner"
+git_cloner_description = "<big>Git cloner</big> \nZadejte URL adresu repositáře, který chcete naklonovat níže:"
+enter_url = "např. https://github.com/example/app"
+clone = "Naklonovat"
+repository_already_cloned = "Repozitář je již naklonovaný. Je v adresáři: %s/Gits"
