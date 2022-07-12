6b5e3efb8d1320262afde39d16046b51246a0035
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-12 21:58:50 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/sh
 git clone https://github.com/vikdevelop/gitcloner-gui /tmp/gitcloner-gui > /dev/null 2>&1
 cd /tmp/gitcloner-gui
-cp main.py ~/.local/bin/gitcloner-gui
+install -D main.py ~/.local/bin/gitcloner-gui
 chmod +x ~/.local/bin/gitcloner-gui
 python3 create-desktop-file.py
 mkdir -p ~/.local/share/gitcloner
 cp -R translations ~/.local/share/gitcloner/
 cp -R icon ~/.local/share/gitcloner/
-cp icon/* ~/.local/share/icons/hicolor/256x256/apps/gitcloner.png
+install -D icon/git.png ~/.local/share/icons/hicolor/256x256/apps/gitcloner.png
 cd
 rm -rf /tmp/gitcloner-gui
