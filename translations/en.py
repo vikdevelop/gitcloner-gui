0dac1ffb68465bf28e15b12ecfa80b085b43248e
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-13 10:55:58 +0200

    Update en.py

--- translations/en.py
+++ translations/en.py
@@ -4,7 +4,7 @@
 repository_is_cloned = 'Repository was cloned!'
 close = "_Close"
 open_folder = "_Open Folder"
-repository_was_cloned_successfully_message = "Repository was cloned successfully! If you want, you can open folder newly-cloned Git repository click on button 'Open Folder'."
+repository_was_cloned_successfully_message = "Repository was cloned successfully! If you want, you can open \nfolder newly-cloned Git repository click on button 'Open Folder'."
 git_cloner_title = "Git cloner"
 git_cloner_description = "<big>Git cloner</big> \nEnter URL of repository want you clone below:"
 enter_url = "e.g. https://github.com/example/app"
