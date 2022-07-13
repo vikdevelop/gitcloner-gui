39aa72b6d822ea01f766204d169ba14ac1bb9084
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-13 10:55:33 +0200

    Update cs.py

--- translations/cs.py
+++ translations/cs.py
@@ -4,7 +4,7 @@
 repository_is_cloned = 'Repozitář byl naklonován!'
 close = "_Zavřít"
 open_folder = "_Otevřít složku"
-repository_was_cloned_successfully_message = "Repozitář byl úspěšně naklonován! Pokud chcete, můžete otevřít složku nově naklonovaného repozitáře kliknutím na tlačítko 'Otevřít složku'."
+repository_was_cloned_successfully_message = "Repozitář byl úspěšně naklonován! Pokud chcete, můžete otevřít \nsložku nově naklonovaného repozitáře kliknutím na tlačítko 'Otevřít složku'."
 git_cloner_title = "Git cloner"
 git_cloner_description = "<big>Git cloner</big> \nZadejte URL adresu repositáře, který chcete naklonovat níže:"
 enter_url = "např. https://github.com/example/app"
