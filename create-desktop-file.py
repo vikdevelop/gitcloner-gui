0002f64dbd1f8b89e6c23bf8c81b9c05b46e6761
vikdevelop <83600218+vikdevelop@users.noreply.github.com>
2022-07-13 10:22:30 +0200

    Update create-desktop-file.py

--- create-desktop-file.py
+++ create-desktop-file.py
@@ -1,4 +1,6 @@
 import os
 HOME = os.path.expanduser('~')
+if not os.path.exists('%s/.local/share/applications' % HOME):
+  os.makedirs('%s/.local/share/applications' % HOME)
 with open('%s/.local/share/applications/com.github.vikdevelop.gitcloner-gui.desktop' % HOME, 'w') as d:
   d.write("[Desktop Entry]\nName=Git cloner\nComment=Simple GUI for download Git repository\nType=Application\nExec=%s/.local/bin/gitcloner-gui\nIcon=gitcloner\nTerminal=false\nCategories=System;System-Tools" % HOME)
